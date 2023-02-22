ce5626dc84445072ada13edaf885c72b42a737c0
mor <mor@LAPTOP-JKSGA5B3>
2023-02-22 14:50:22 +0200

    Geometries finished

---
+++ src/geometries/Sphere.java
@@ -0,0 +1,23 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Vector;
+/**
+ *Spgere
+ *
+ * @author Mor Sharoni
+ */
+public class Sphere implements Geometry{
+    final Point center;
+    final double radius;
+
+    public Sphere(Point center, double radius) {//simple constructor
+        this.center = center;
+        this.radius = radius;
+    }
+
+    @Override
+    public Vector getNormal(Point point) {
+        return null;
+    }
+}
---
+++ src/geometries/Tube.java
@@ -0,0 +1,25 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Vector;
+import primitives.Ray;
+/**
+ *Tube
+ *
+ * @author Mor Sharoni
+ */
+public class Tube implements Geometry{
+
+    protected Ray axisRay;
+    protected double radius;
+
+    public Tube(Ray axisRay, double radius) {//simple constructor
+        this.axisRay = axisRay;
+        this.radius = radius;
+    }
+
+    @Override
+    public Vector getNormal(Point point) {
+        return null;
+    }
+}
